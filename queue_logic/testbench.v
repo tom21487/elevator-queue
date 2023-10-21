b575c7eb5e63edc63c407d19e686e72fd493fba5
Tom Jiao <tom21487@outlook.com>
2023-10-21 13:31:24 -0400

    Updated queue_logic testbench.

--- queue_logic/testbench.v
+++ queue_logic/testbench.v
@@ -64,93 +64,157 @@ module testbench();
 	end
     endtask
 
-    // Toggle inputs (don't change!!)
+    // Toggle inputs
     initial begin
-	// Test 0: add pressed_lvl
+	/*
+     Test 0: add pressed_lvl
+     Before:
+     - Current state: A
+     - Queued states: [ B, C ]
+     - State to add: D
+     After:
+     - Current state in queue: no
+     - Queued states: [ B, C, D ]     
+    */
 	queue = { A, D, C, B };
 	tail = 2;
-        pos_lvl = A;
-        pressed_en = 1;
-        pressed_lvl = D;
-	next_queue_sub_expected = { A, D, C, B };
-        next_tail_sub_expected = 3;
-        stop_at_pos_lvl_expected = 0;
+    pos_lvl = A;
+    pressed_en = 1;
+    pressed_lvl = D;
+    next_queue_sub_expected = { A, D, C, B };
+    next_tail_sub_expected = 3;
+    stop_at_pos_lvl_expected = 0;
 	#10;
 	check_ans();
 
-	// Test 1: subtract pos_lvl
+	/*
+     Test 1: subtract pos_lvl
+     Before:
+     - Current state: C
+     - Queued states: [ A, C, B ]
+     - State to add: none
+     After:
+     - Current state in queue: yes
+     - Queued states: [ A, B ]
+    */
 	queue = { D, B, C, A };
 	tail = 3;
-        pos_lvl = C;
-        pressed_en = 0;
-        pressed_lvl = D;
+    pos_lvl = C;
+    pressed_en = 0;
+    pressed_lvl = D;
 	next_queue_sub_expected = { D, D, B, A };
-        next_tail_sub_expected = 2;
-        stop_at_pos_lvl_expected = 1;
+    next_tail_sub_expected = 2;
+    stop_at_pos_lvl_expected = 1;
 	#10;
 	check_ans();
 
-	// Test 2: don't add because pressed_lvl_in_queue
+	/*
+     Test 2: don't add because pressed_lvl_in_queue
+     Before:
+     - Current state: C
+     - Queued states: [ A ]
+     - State to add: A
+     After:
+     - Current state in queue: no
+     - Queued states: [ A ]
+    */
 	queue = { D, B, C, A };
 	tail = 1;
-        pos_lvl = C;
-        pressed_en = 0;
-        pressed_lvl = A;
+    pos_lvl = C;
+    pressed_en = 1;
+    pressed_lvl = A;
 	next_queue_sub_expected = { D, B, C, A };
-        next_tail_sub_expected = 1;
-        stop_at_pos_lvl_expected = 0;
+    next_tail_sub_expected = 1;
+    stop_at_pos_lvl_expected = 0;
 	#10;
 	check_ans();
 
-	// Test 3: don't subtract because tail is out of bounds
+	/*
+     Test 3: don't subtract because tail is out of bounds
+     Before:
+     - Current state: D
+     - Queued states: [ A ]
+     - State to add: none
+     After:
+     - Current state in queue: no
+     - Queued states: [ A ]
+    */
 	queue = { B, D, C, A };
 	tail = 1;
-        pos_lvl = D;
-        pressed_en = 0;
-        pressed_lvl = B;
+    pos_lvl = D;
+    pressed_en = 0;
+    pressed_lvl = B;
 	next_queue_sub_expected = { B, D, C, A };
-        next_tail_sub_expected = 1;
-        stop_at_pos_lvl_expected = 0;
+    next_tail_sub_expected = 1;
+    stop_at_pos_lvl_expected = 0;
 	#10;
 	check_ans();
 
-	// Test 4: add and subtract at the same time (different levels)
+	/*
+     Test 4: add and subtract at the same time (different levels)
+     Input:
+     - Current state: B
+     - Queued states: [ B ]
+     - States to add: A
+     Output:
+     - Current state in queue: yes
+     - Queued states: [ A ]
+    */
 	queue = { A, D, C, B };
 	tail = 1;
-        pos_lvl = B;
-        pressed_en = 1;
-        pressed_lvl = A;
+    pos_lvl = B;
+    pressed_en = 1;
+    pressed_lvl = A;
 	next_queue_sub_expected = { A, A, D, A };
-        next_tail_sub_expected = 1;
-        stop_at_pos_lvl_expected = 1;
+    next_tail_sub_expected = 1;
+    stop_at_pos_lvl_expected = 1;
 	#10;
 	check_ans();
 
-	// Test 5: add and subtract at the same time (same level, full)
+	/*
+     Test 5: add and subtract at the same time (same level, full)
+     Input: 
+     - Current state: C
+     - Queued states: [ D, C, A, B ]
+     - State to add: C
+     Output:
+     - Current state in queue: yes
+     - Queued states: [ D, A, B ]
+    */
 	queue = { B, A, C, D };
 	tail = 4;
-        pos_lvl = C;
-        pressed_en = 1;
-        pressed_lvl = C;
+    pos_lvl = C;
+    pressed_en = 1;
+    pressed_lvl = C;
 	next_queue_sub_expected = { B, B, A, D };
-        next_tail_sub_expected = 3;
-        stop_at_pos_lvl_expected = 1;
+    next_tail_sub_expected = 3;
+    stop_at_pos_lvl_expected = 1;
 	#10;
 	check_ans();
 
-	// Test 6: add and subtract at the same time (same level, not full)
+	/*
+     Test 6: add and subtract at the same time (same level, not full)
+     Input:
+     - Current state: C
+     - Queued states: [ D, C, A ]
+     - State to add: C
+     Output:
+     - Current state in queue: yes
+     - Queued state: [ D, A ]
+    */
 	queue = { B, A, C, D };
-	tail = 4;
-        pos_lvl = C;
-        pressed_en = 1;
-        pressed_lvl = C;
+	tail = 3;
+    pos_lvl = C;
+    pressed_en = 1;
+    pressed_lvl = C;
 	next_queue_sub_expected = { B, B, A, D };
-        next_tail_sub_expected = 3;
-        stop_at_pos_lvl_expected = 1;
+    next_tail_sub_expected = 2;
+    stop_at_pos_lvl_expected = 1;
 	#10;
 	check_ans();
-	// 5和6告诉我们应该先减再加，之后再改。
+
     end
+
     // Run simulation (output to .vcd file)
     initial begin
         // Create simulation output file
